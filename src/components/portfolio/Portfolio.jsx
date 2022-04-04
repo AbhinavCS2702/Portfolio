11c4300374f587902276bca2cfce2b6ecc8f7f14
AbhinavCS2702 <1602-19-737-123@vce.ac.in>
2022-04-04 18:08:00 +0530

    first push

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,9 @@
+import "./Portfolio.scss"
+
+function Portfolio() {
+  return (
+    <div className="portfolio" id="portfolio"></div>
+  )
+}
+
+export default Portfolio
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import "./topbar.scss"
+function Topbar() {
+  return (
+    <div className="topbar">
+        <div className="wrapper">
+            <div className="left">
+                <a href="#intro">asdasd</a>
+            </div>
+            <div className="right"></div>
+            
+        </div>
+    </div>
+
+  )
+}
+
+export default Topbar;
